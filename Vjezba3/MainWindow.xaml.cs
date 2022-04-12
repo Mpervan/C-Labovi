361860162d8db072a1ed85b757912d0bc841bc74
Mpervan <pervan.matea7@gmail.com>
2022-04-12 19:41:08 +0200

    Treca vjezba

---
+++ Vjezba3/MainWindow.xaml.cs
@@ -0,0 +1,90 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using System.Globalization;
+using System.Collections;
+using System.Collections.ObjectModel;
+
+namespace Vjezba3
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        static int Brojac = 0;
+        static Collection<Uplatnica> Transakcije = new Collection<Uplatnica>();
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+        static bool Hitno;
+        private void HitnoBox_Checked(object sender, RoutedEventArgs e)
+        {
+            Hitno = true;
+        }
+        private void Uplati_Botun_Klik(object sender, RoutedEventArgs e)
+        {
+            if (Platitelj_Ime.Text == "" || Valuta.Text == "" || IznosTekst.Text == "" || IBANPlatitelj.Text == "" || ModelPlatitelj.Text == "" || PrimateljIme.Text == "" || PrimateljIBAN.Text == "" || PrimateljModel.Text == "")
+            {
+                MessageBox.Show("Unesite podatke prije spremanja!", "Greška!");
+                return;
+            }
+            double Test;
+            if (double.TryParse(IznosTekst.Text,out Test) == false)
+            {
+                MessageBox.Show("Novčani iznos nije broj!", "Greška!");
+                return;
+            }
+            if(IBANPlatitelj.Text == PrimateljIBAN.Text)
+            {
+                MessageBox.Show("IBAN platitelja je identičan IBAN-u primatelja.", "Greška!");
+                return;
+            }
+
+            Uplatnica Nova = new Uplatnica(Platitelj_Ime.Text, Valuta.Text, double.Parse(IznosTekst.Text), ModelPlatitelj.Text, PNBPlatitelj.Text, IBANPlatitelj.Text, PrimateljIme.Text, PrimateljModel.Text, PrimateljPNB.Text, PrimateljIBAN.Text, Hitno);
+            if (Nova.Provjere() == false)
+            {
+                MessageBox.Show("Jedan ili više podataka je unešeno \nu neispravnom formatu! \n \n....Pokušajte ponovno!....", "Greška!");
+                return;
+            }
+            if (Nova.Provjere() == true)
+            {
+                Brojac += 1;
+                Nova.Broj = Brojac;
+                Transakcije.Add(Nova);
+                MessageBox.Show("Uplatili ste: " + IznosTekst.Text + Valuta.Text + "\nsa računa pod IBAN-om: " + IBANPlatitelj.Text + "\nna račun pod IBAN-om: " + PrimateljIBAN.Text, "Transakcija uspješna");
+                IznosTekst.Clear();
+
+            }
+        }
+
+        private void Pronadi_IBAN(object sender, RoutedEventArgs e)
+        {
+            foreach(Uplatnica P in Transakcije)
+            {
+                P.PretraziIBAN(IBANPlatitelj.Text);
+            }
+            
+        }
+
+        private void Pronadi_Ime(object sender, RoutedEventArgs e)
+        {
+            foreach (Uplatnica P in Transakcije)
+            {
+                P.PretraziIme(Platitelj_Ime.Text);
+            }
+        }
+    }
+}
---
+++ Vjezba3/Properties/Uplatnica.cs
@@ -0,0 +1,86 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Globalization;
+using System.Windows;
+
+namespace Vjezba3
+{
+    class Uplatnica 
+    {
+        int Br;
+        string Platitelj;
+        string Valuta;
+        double Iznos;
+        string ModelPlatitelj;
+        string PozivNaBrojPlatitelja;
+        string IBANPlatitelja;
+        string Primatelj;
+        string ModelPrimatelj;
+        string PozivNaBrojPrimatelja;
+        string IBANPrimatelja;
+        bool Hitno;
+        DateTime Datum;
+        public Uplatnica(string P, string V, double I, string M1, string PNB1, string IBAN1, string P2, string M2, string PNB2, string IBAN2, bool H)
+        {
+            this.Platitelj = P;
+            this.Valuta = V;
+            this.Iznos = I;
+            this.ModelPlatitelj = M1;
+            this.PozivNaBrojPlatitelja = PNB1;
+            this.IBANPlatitelja = IBAN1;
+            this.Primatelj = P2;
+            this.ModelPrimatelj = M2;
+            this.PozivNaBrojPrimatelja = PNB2;
+            this.IBANPrimatelja = IBAN2;
+            this.Datum = DateTime.Now;
+        }
+        public int Broj
+        {
+            get
+            {
+                return this.Br;
+            }
+            set
+            {
+                this.Br = value;
+            }
+        }
+
+        public void IspisiUplatnicu()
+        {
+            MessageBox.Show("\n \n Transakcija broj " + Br + " \n Platitelj: " + this.Platitelj + "\n Valuta: " + this.Valuta + " Iznos: " + this.Iznos.ToString() + "\n Model Platitelja: " + this.ModelPlatitelj + "   Poziv na broj: " + this.PozivNaBrojPlatitelja + "\n IBAN: " + this.IBANPlatitelja + "\n Model Primatelja: " + this.ModelPrimatelj + "   Poziv na broj: " + this.PozivNaBrojPrimatelja + "\n IBAN: " + this.IBANPrimatelja + "\n DAtum transakcije: " + Datum.ToString() + "\n \n");
+        }
+            
+        public void PretraziIBAN(string IBAN)
+        {
+            if (this.IBANPlatitelja == IBAN || this.IBANPrimatelja == IBAN)
+                this.IspisiUplatnicu();
+        }
+        public void PretraziIme(string Ime)
+        {
+            if (this.Platitelj == Ime || this.Primatelj == Ime)
+                this.IspisiUplatnicu();
+        }
+        public bool Provjere()
+        {
+            if (this.ModelPlatitelj.Length != 4 || this.ModelPlatitelj.IndexOf('H') != 0 || this.ModelPlatitelj.IndexOf('R') != 1 || !Char.IsNumber(this.ModelPlatitelj, 2) || !Char.IsNumber(this.ModelPlatitelj, 2))
+                return false;
+            if (this.PozivNaBrojPlatitelja.Length > 22)
+                return false;
+            if (this.IBANPlatitelja.Length != 21 || this.IBANPlatitelja.IndexOf('H') != 0 || this.IBANPlatitelja.IndexOf('R') != 1)
+                return true;
+            if (this.ModelPrimatelj.Length != 4 || this.ModelPrimatelj.IndexOf('H') != 0 || this.ModelPrimatelj.IndexOf('R') != 1 || !Char.IsNumber(this.ModelPrimatelj, 2) || !Char.IsNumber(this.ModelPrimatelj, 2))
+                return false;
+            if (this.PozivNaBrojPrimatelja.Length > 22)
+                return false;
+            if (this.IBANPrimatelja.Length != 21 || this.IBANPrimatelja.IndexOf('H') != 0 || this.IBANPrimatelja.IndexOf('R') != 1)
+                return false;
+            else;
+            return true;
+
+        }
+    }
+}
